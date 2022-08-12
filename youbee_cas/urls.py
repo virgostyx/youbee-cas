e48acf8041c7ce8c41ca1d82a418f6665297795f
webmaster.youbee <webmaster.youbee@gmail.com>
2022-08-12 14:32:11 +0000

    change admin url

--- youbee_cas/urls.py
+++ youbee_cas/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('olympus/', admin.site.urls),
     path(r'', include('mama_cas.urls')),
 ]
